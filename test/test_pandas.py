87deeafdac17f448fad62b9cb1da5d68c2bcce5a
Shumpei Shiina <shumpei_shiina@toyota-tokyo.tech>
2024-10-30 16:02:54 +0900

    add comments to test

--- test/test_pandas.py
+++ test/test_pandas.py
@@ -17,6 +17,7 @@ def load_dataframe():
 def test_dataframe_size():
     pdf, df = load_dataframe()
 
+    # The `shape` member should be a pair of a sensitive value (row) and a non-sensitive value (column)
     assert isinstance(pdf.shape, tuple)
     assert len(pdf.shape) == len(df.shape) == 2
     assert isinstance(pdf.shape[0], pripri.Prisoner)
@@ -24,15 +25,19 @@ def test_dataframe_size():
     assert pdf.shape[0].sensitivity == 1
     assert pdf.shape[1] == len(pdf.columns) == len(df.columns)
 
+    # The `size` member should be a sensitive value
     assert isinstance(pdf.size, pripri.Prisoner)
     assert pdf.size._value == df.size
     assert pdf.size.sensitivity == len(pdf.columns) == len(df.columns)
 
-    with pytest.raises(pripri.DPError): len(pdf)
+    # Builtin `len()` function should raise an error because it must be an integer value
+    with pytest.raises(pripri.DPError):
+        len(pdf)
 
 def test_dataframe_comp():
     pdf, df = load_dataframe()
 
+    # A non-sensitive value should be successfully compared against a sensitive dataframe
     assert isinstance(pdf == 3, ppd.DataFrame)
     assert isinstance(pdf != 3, ppd.DataFrame)
     assert isinstance(pdf <  3, ppd.DataFrame)
@@ -46,6 +51,7 @@ def test_dataframe_comp():
     assert ((pdf >  3)._value == (df >  3)).all().all()
     assert ((pdf >= 3)._value == (df >= 3)).all().all()
 
+    # A non-sensitive value should be successfully compared against a sensitive series
     assert isinstance(pdf["a"] == 3, ppd.Series)
     assert isinstance(pdf["a"] != 3, ppd.Series)
     assert isinstance(pdf["a"] <  3, ppd.Series)
@@ -59,6 +65,7 @@ def test_dataframe_comp():
     assert ((pdf["a"] >  3)._value == (df["a"] >  3)).all()
     assert ((pdf["a"] >= 3)._value == (df["a"] >= 3)).all()
 
+    # An irrelevant, non-sensitive dataframe should not be compared against a sensitive dataframe
     with pytest.raises(pripri.DPError): pdf == df
     with pytest.raises(pripri.DPError): pdf != df
     with pytest.raises(pripri.DPError): pdf <  df
@@ -66,6 +73,15 @@ def test_dataframe_comp():
     with pytest.raises(pripri.DPError): pdf >  df
     with pytest.raises(pripri.DPError): pdf >= df
 
+    # An irrelevant, non-sensitive 2d array should not be compared against a sensitive dataframe
+    with pytest.raises(pripri.DPError): pdf == [[list(range(len(pdf.columns)))] for x in range(5)]
+    with pytest.raises(pripri.DPError): pdf != [[list(range(len(pdf.columns)))] for x in range(5)]
+    with pytest.raises(pripri.DPError): pdf <  [[list(range(len(pdf.columns)))] for x in range(5)]
+    with pytest.raises(pripri.DPError): pdf <= [[list(range(len(pdf.columns)))] for x in range(5)]
+    with pytest.raises(pripri.DPError): pdf >  [[list(range(len(pdf.columns)))] for x in range(5)]
+    with pytest.raises(pripri.DPError): pdf >= [[list(range(len(pdf.columns)))] for x in range(5)]
+
+    # An irrelevant, non-sensitive array should not be compared against a sensitive series
     with pytest.raises(pripri.DPError): pdf["a"] == [0, 1, 2, 3, 4]
     with pytest.raises(pripri.DPError): pdf["a"] != [0, 1, 2, 3, 4]
     with pytest.raises(pripri.DPError): pdf["a"] <  [0, 1, 2, 3, 4]
@@ -75,6 +91,7 @@ def test_dataframe_comp():
 
     x = pripri.Prisoner(value=0, sensitivity=1)
 
+    # A sensitive value should not be compared against a sensitive dataframe
     with pytest.raises(pripri.DPError): pdf == x
     with pytest.raises(pripri.DPError): pdf != x
     with pytest.raises(pripri.DPError): pdf <  x
@@ -82,6 +99,7 @@ def test_dataframe_comp():
     with pytest.raises(pripri.DPError): pdf >  x
     with pytest.raises(pripri.DPError): pdf >= x
 
+    # A sensitive value should not be compared against a sensitive series
     with pytest.raises(pripri.DPError): pdf["a"] == x
     with pytest.raises(pripri.DPError): pdf["a"] != x
     with pytest.raises(pripri.DPError): pdf["a"] <  x
@@ -92,104 +110,168 @@ def test_dataframe_comp():
 def test_dataframe_getitem():
     pdf, df = load_dataframe()
 
+    # A single-column view should be successfully retrieved from a sensitive dataframe
     assert isinstance(pdf["a"], ppd.Series)
     assert (pdf["a"]._value == df["a"]).all()
 
+    # A multi-column view should be successfully retrieved from a sensitive dataframe
     assert isinstance(pdf[["a", "b"]], ppd.DataFrame)
     assert (pdf[["a", "b"]]._value == df[["a", "b"]]).all().all()
 
-    x = pripri.Prisoner(value=0, sensitivity=1)
-    with pytest.raises(pripri.DPError): pdf[x]
+    # An irrelevant, non-sensitve bool vector should not be accepted for filtering (dataframe)
+    with pytest.raises(pripri.DPError):
+        pdf[[True, True, False, False, True]]
 
-    with pytest.raises(pripri.DPError): pdf[2:5]
-    with pytest.raises(pripri.DPError): pdf[[True, True, False, False, True]]
+    # An irrelevant, non-sensitve bool vector should not be accepted for filtering (series)
+    with pytest.raises(pripri.DPError):
+        pdf["a"][[True, True, False, False, True]]
 
+    # A bool-filtered view should be successfully retrieved from a sensitive dataframe
     assert isinstance(pdf[pdf["a"] > 3], ppd.DataFrame)
     assert (pdf[pdf["a"] > 3]._value == df[df["a"] > 3]).all().all()
 
-    with pytest.raises(pripri.DPError): pdf["a"][2:5]
-    with pytest.raises(pripri.DPError): pdf["a"][[True, True, False, False, True]]
-
+    # A bool-filtered view should be successfully retrieved from a sensitive series
     assert isinstance(pdf["a"][pdf["a"] > 3], ppd.Series)
     assert (pdf["a"][pdf["a"] > 3]._value == df["a"][df["a"] > 3]).all()
+
+    x = pripri.Prisoner(value=0, sensitivity=1)
+
+    # A sensitive value should not be used as a column name
+    with pytest.raises(pripri.DPError):
+        pdf[x]
+
+    # A slice should not be used for selecting rows of a dataframe
+    # TODO: it might be legal to accept a slice
+    with pytest.raises(pripri.DPError):
+        pdf[2:5]
+
+    # A slice should not be used for selecting rows of a series
+    # TODO: it might be legal to accept a slice
+    with pytest.raises(pripri.DPError):
+        pdf["a"][2:5]
 
 def test_dataframe_setitem():
     pdf, df = load_dataframe()
 
+    # A non-sensitive value should be successfully assigned to a single-column view
     pdf["c"] = 10
     df["c"] = 10
     assert (pdf.columns == df.columns).all()
     assert (pdf._value == df).all().all()
 
+    # A sensitive series should be successfully assigned to a single-column view
     pdf["d"] = pdf["a"]
     df["d"] = df["a"]
     assert (pdf.columns == df.columns).all()
     assert (pdf._value == df).all().all()
 
+    # A non-sensitive value should be successfully assigned to a multi-column view
     pdf[["e", "f"]] = 100
     df[["e", "f"]] = 100
     assert (pdf.columns == df.columns).all()
     assert (pdf._value == df).all().all()
 
+    # A sensitive dataframe should be successfully assigned to a multi-column view
     pdf[["g", "h"]] = pdf[["a", "b"]]
     df[["g", "h"]] = df[["a", "b"]]
     assert (pdf.columns == df.columns).all()
     assert (pdf._value == df).all().all()
 
+    # A non-sensitive value should be successfully assigned to a bool-filtered view
     pdf[pdf["a"] < 3] = 8
     df[df["a"] < 3] = 8
     assert (pdf.columns == df.columns).all()
     assert (pdf._value == df).all().all()
 
+    # A non-sensitive array should be successfully assigned to a bool-filtered view
     pdf[pdf["a"] == 8] = list(range(len(pdf.columns)))
     df[df["a"] == 8] = list(range(len(df.columns)))
     assert (pdf.columns == df.columns).all()
     assert (pdf._value == df).all().all()
 
+    # A sensitive dataframe should be successfully assigned to a bool-filtered view
     pdf[pdf["a"] < 3] = pdf[pdf["a"] > 3].reset_index(drop=True)
     df[df["a"] < 3] = df[df["a"] > 3].reset_index(drop=True)
     assert (pdf.columns == df.columns).all()
     assert (pdf._value == df).all().all()
 
+    # A non-sensitive series can be assigned to a sensitive single-column view
+    # (because this operation succeeds regardless of the row number)
+    # TODO: this may be disallowed if we track the value domain for each column
     pdf["i"] = df["c"]
     df["i"] = df["c"]
     assert (pdf.columns == df.columns).all()
     assert (pdf._value == df).all().all()
 
+    # A non-sensitive dataframe can be assigned to a sensitive multi-column view
+    # (because this operation succeeds regardless of the row number)
+    # TODO: this may be disallowed if we track the value domain for each column
     pdf[["j", "k"]] = df[["a", "b"]]
     df[["j", "k"]] = df[["a", "b"]]
     assert (pdf.columns == df.columns).all()
     assert (pdf._value == df).all().all()
 
+    # An irrelevant, non-sensitive dataframe can be assigned to a bool-filtered view
+    # (because this operation succeeds regardless of the row number)
+    # TODO: this may be disallowed if we track the value domain for each column
     pdf[pdf["a"] > 3] = df
     df[df["a"] > 3] = df
     assert (pdf.columns == df.columns).all()
     assert (pdf._value == df).all().all()
 
     x = pripri.Prisoner(value=0, sensitivity=1)
-    with pytest.raises(pripri.DPError): pdf["x"] = x
-    with pytest.raises(pripri.DPError): pdf["x"] = [1, 2, 3, 4, 5]
-    with pytest.raises(pripri.DPError): pdf[["x", "y"]] = x
-    with pytest.raises(pripri.DPError): pdf[pdf["a"] > 3] = x
-    with pytest.raises(pripri.DPError): pdf[pdf["a"] > 3] = [[list(range(len(pdf.columns)))] for x in range(5)]
-    with pytest.raises(pripri.DPError): pdf[x] = 10
-    with pytest.raises(pripri.DPError): pdf[2:5] = 0
+
+    # A sensitive value should not be assigned to a single-column view
+    with pytest.raises(pripri.DPError):
+        pdf["x"] = x
+
+    # A sensitive value should not be assigned to a multi-column view
+    with pytest.raises(pripri.DPError):
+        pdf[["x", "y"]] = x
+
+    # A sensitive value should not be assigned to a bool-filtered view
+    with pytest.raises(pripri.DPError):
+        pdf[pdf["a"] > 3] = x
+
+    # An irrelevant, non-sensitive array should not be assigned to a column view
+    with pytest.raises(pripri.DPError):
+        pdf["x"] = [1, 2, 3, 4, 5]
+
+    # An irrelevant, non-sensitive 2d array should not be assigned to a bool-filtered view
+    with pytest.raises(pripri.DPError):
+        pdf[pdf["a"] > 3] = [[list(range(len(pdf.columns)))] for x in range(5)]
+
+    # A sensitive value should not be used as a column name
+    with pytest.raises(pripri.DPError):
+        pdf[x] = 10
+
+    # A slice should not be used for selecting rows
+    # TODO: it might be legal to accept a slice
+    with pytest.raises(pripri.DPError):
+        pdf[2:5] = 0
 
 def test_dataframe_reset_index():
     pdf, df = load_dataframe()
 
+    # Default behaviour
     assert (pdf.reset_index()._value == df.reset_index()).all().all()
 
+    # Special behaviour when `inplace=True`
     assert pdf.reset_index(inplace=True) == None
     assert (pdf._value == df.reset_index()).all().all()
 
 def test_series_reset_index():
     pdf, df = load_dataframe()
 
+    # Default behaviour
     assert isinstance(pdf["a"].reset_index(), ppd.DataFrame)
     assert (pdf["a"].reset_index()._value == df["a"].reset_index()).all().all()
+
+    # Special behaviour when `drop=True`
     assert isinstance(pdf["a"].reset_index(drop=True), ppd.Series)
     assert (pdf["a"].reset_index(drop=True)._value == df["a"].reset_index(drop=True)).all().all()
+
+    # Special behaviour when `drop=True` and `inplace=True`
     assert pdf["a"].reset_index(drop=True, inplace=True) == None
     assert df["a"].reset_index(drop=True, inplace=True) == None
     assert (pdf._value == df).all().all()
