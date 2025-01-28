16c7f5cc22355eca21771e26f07368645bbc55ff
Shumpei Shiina <shumpei_shiina@toyota-tokyo.tech>
2025-01-28 13:23:08 +0900

    [pandas] egrpc support for exponential mechanism

--- src/privjail/mechanism.py
+++ src/privjail/mechanism.py
@@ -39,23 +39,20 @@ def exponential_mechanism(scores: list[SensitiveInt | SensitiveFloat], eps: floa
 def exponential_mechanism(scores: dict[T, SensitiveInt | SensitiveFloat], eps: float) -> T: ...
 
 def exponential_mechanism(scores: list[SensitiveInt | SensitiveFloat] | dict[Any, SensitiveInt | SensitiveFloat], eps: float) -> Any:
-    if len(scores) == 0:
-        raise ValueError("scores must have at least one element.")
-
     if isinstance(scores, dict):
-        keys = list(scores.keys())
-        values = list(scores.values())
+        idx = _exponential_mechanism_impl(list(scores.values()), eps)
+        return list(scores.keys())[idx]
     elif isinstance(scores, list):
-        keys = list(range(len(scores)))
-        values = scores
+        return _exponential_mechanism_impl(scores, eps)
     else:
         raise ValueError("scores must be a list or dict.")
 
-    for v in values:
-        if not isinstance(v, (SensitiveInt, SensitiveFloat)):
-            raise TypeError("values of scores must be SensitiveInt or SensitiveFloat.")
+@egrpc.function
+def _exponential_mechanism_impl(scores: list[SensitiveInt | SensitiveFloat], eps: float) -> int:
+    if len(scores) == 0:
+        raise ValueError("scores must have at least one element.")
 
-    sensitivity = max([v.distance.max() for v in values]) # type: ignore[type-var]
+    sensitivity = max([v.distance.max() for v in scores]) # type: ignore[type-var]
 
     if sensitivity <= 0:
         raise DPError(f"Invalid sensitivity ({sensitivity})")
@@ -64,12 +61,12 @@ def exponential_mechanism(scores: list[SensitiveInt | SensitiveFloat] | dict[Any
         raise DPError(f"Invalid epsilon ({eps})")
 
     # create a dummy prisoner to propagate budget consumption to all prisoners
-    prisoner_dummy = Prisoner(0, values[0].distance, parents=values)
+    prisoner_dummy = Prisoner(0, scores[0].distance, parents=scores)
     prisoner_dummy.consume_privacy_budget(eps)
 
-    exponents = [eps * v._value / sensitivity / 2 for v in values]
+    exponents = [eps * s._value / sensitivity / 2 for s in scores]
     # to prevent too small or large values (-> 0 or inf)
     M: float = _np.max(exponents) # type: ignore[arg-type]
     p = [_np.exp(x - M) for x in exponents]
     p /= sum(p)
-    return _np.random.choice(keys, p=p)
+    return _np.random.choice(len(scores), p=p)
