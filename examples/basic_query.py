bc9d208411806efa41c1778c0af245a32b888bcd
Shumpei Shiina <shumpei_shiina@toyota-tokyo.tech>
2025-10-24 13:59:16 +0900

    minor fix

--- examples/basic_query.py
+++ examples/basic_query.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import argparse
 import privjail as pj
 import privjail.pandas as ppd
